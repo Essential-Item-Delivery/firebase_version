51a4e7916737c6903ec586da264a9b55089c4da3
Yuyang Zou <zoude603@student.otago.ac.nz>
2020-08-07 15:17:57 +1200

    add function: display username after login

--- js/check_login.js
+++ js/check_login.js
@@ -7,18 +7,44 @@
 var checklogin = (function () {
     'use strict';
     //global variables
-    var pub = {};
+    var pub = {},tokon;
 
 
 
     //setup public
     pub.setup = function () {
 
-       var tokon = Cookie.get("loginToken");
-       if(tokon!="" || tokon!=null){
 
+        var
+
+
+        tokon = Cookie.get("loginToken");
+        console.log(tokon);
+
+       if(tokon==="" || tokon===null){
+
+            console.log("not login")
 
        }
+       else if(tokon==="Demo account"){
+
+
+            console.log("log in")
+
+          var div= document.getElementsByClassName("header__top__right__auth");
+
+           console.log( div);
+
+            div[0].innerHTML='<a href="#"><i class="fa fa-user">' +tokon+'</i> </a>';
+           div[1].innerHTML='<a href="#"><i class="fa fa-user">' +tokon+'</i> </a>';
+          // div.innerHTML="<p>asdasdasdadsasdasdasdasd</p>";
+
+
+           console.log( document.getElementById("divlogin"))
+
+        }
+
+
 
     };
 
