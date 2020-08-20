9d9d59b1216372097f4100d19f849a50359076a4
Raiiiden <55169059+Raiiiden@users.noreply.github.com>
2020-08-21 11:51:28 +1200

    Made changes

--- public/js/functions.js
+++ public/js/functions.js
@@ -2,9 +2,13 @@
 const email = document.getElementById('input-Email');
 const password = document.getElementById('input-Password');
 
+document.getElementById('btnlogin').onclick = login();
+
+
 // Add login event
 function login() {
     firebase.auth().signInWithEmailAndPassword(email.value, password.value).catch(function(error) {
+        window.alert("Clicked");
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
