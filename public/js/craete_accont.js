792ac40cbb1bd34ba35afa711172e246d7c41fae
DerekZou (laptop) <zoude603@student.otago.ac.nz>
2020-08-24 15:09:41 +1200

    js update

--- public/js/craete_accont.js
+++ public/js/craete_accont.js
@@ -1,55 +1,52 @@
-
-
 var account = (function () {
-    'use strict';
-    //global variables
-    var pub = {},submit;
-   
-    //click
-       function create() {
-          const auth = firebase.auth();
-            var email = $('this').child.$('#firstname');
+   'use strict';
+   //global variables
+   var pub = {}, submit;
+
+   //click
+   function create() {
+      const auth = firebase.auth();
+      var email = $('this').child.$('#firstname');
 
 
-       }
-   
-    
-   
-    //setup public
-    pub.setup = function () {
-   
-   var test =$('#create_account_form').submit(create);
-   console.log(test);
-    };
-   
-    return pub;
-   
-   
-   }());
-   
-   
-   
-   if (window.addEventListener) {
-    window.addEventListener("load", account.setup);
-   } else if (window.attachEvent) {
-    window.attachEvent("onload", account.setup);
-   } else {
-    alert("Could not attach ’MovieCategories.setup’ to the ’window.onload’ event");
    }
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
+
+
+
+   //setup public
+   pub.setup = function () {
+
+      var test = $('#create_account_form').submit(create);
+      console.log(test);
+   };
+
+   return pub;
+
+
+}());
+
+
+
+if (window.addEventListener) {
+   window.addEventListener("load", account.setup);
+} else if (window.attachEvent) {
+   window.attachEvent("onload", account.setup);
+} else {
+   alert("Could not attach ’MovieCategories.setup’ to the ’window.onload’ event");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
