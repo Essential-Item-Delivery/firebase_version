186aea1e209fca0bdea9747046e50e9be40278ad
Derek Zou <zoude603@student.otago.ac.nz>
2020-09-04 01:02:46 +1200

    data control to display username

--- public/js/index.js
+++ public/js/index.js
@@ -20,13 +20,18 @@ var indexControl = (function () {
     //global variables
     var pub = {};
     
-    pub.displayUser = function(){
+    pub.displayUser = async function(){
         $("#loggerRemove").remove();
         $("#createrRemove").remove();
+
         console.log(firebase.auth().currentUser.uid);
+
         var uid = firebase.auth().currentUser.uid;
-        console.log("result is"+datacontrol.getUserName(uid));
-        $("#logger").html('<i class=\"fa fa-user\">Welcome ' + datacontrol.getUserName(uid) +'</i>');
+
+       // console.log("result is"+datacontrol.getUserName(uid));
+
+        $("#logger").html('<i class=\"fa fa-user\">Welcome ' +await datacontrol.getUserName(uid) +'</i>');
+        console.log("function called");
     }
 
     //set  drop down list to database variables
