f3394d97221d93b57c452885a25590cfc6ce449a
LittleRookie1091 <akatsuki1091@gmail.com>
2020-09-03 22:43:49 +1200

    Added indexControl

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,18 @@
 /**
  * Created by yzou on 8/7/20.
  */
+
+var indexControl = (function () {
+
+    //global variables
+    var pub = {};
+
+    pub.displayUser = function(){
+        $("#loggerRemove").remove();
+        $("#createrRemove").remove();
+        $("#logger").html('<i class=\"fa fa-user\">Welcome '+firebase.auth().currentUser.uid+'</i>');
+    }
+
+    return pub;
+
+}());
