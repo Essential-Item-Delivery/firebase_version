a4c9e80c597e8b23334607ab748c26c1fd73544f
LittleRookie1091 <akatsuki1091@gmail.com>
2020-09-10 21:14:56 +1200

    Changed Checkout to suit shopperly style

---
+++ public/js/shop.js
@@ -0,0 +1,23 @@
+var shopControl = (function () {
+    // Get a reference to the database service
+    var db = firebase.database();
+    //global variables
+    var pub = {};
+
+    pub.setShop = (function(){
+
+    });
+
+    //setup public
+    pub.setup = function () {
+
+
+    };
+
+    return pub;
+
+}());
+
+
+
+//$(document).ready(productControl.getProduct());
