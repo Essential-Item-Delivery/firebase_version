ce273da0a7984f5958f899659edf7999a87221bb
Derek Zou <zoude603@student.otago.ac.nz>
2020-09-18 01:30:22 +1200

    adding URL of the img

--- public/js/index.js
+++ public/js/index.js
@@ -23,13 +23,22 @@ var indexControl = (function () {
         //get array of all shop name        
         var shops =  Object.keys(t.val());
 
-        for(var i =0 ; i<shops.length; i++){
+        for(var i =0 ; i<shops.length-1; i++){
             console.log(shops[i]);
+            for(var j =3; j<6; j++){
+               var p =Object.entries(t.val())[i][1][j];
+               console.log(p);
+               var url = await firebase.storage().ref("/images/"+shops[i]+"/Product/product"+p.ProductID+".png").getDownloadURL();
+                makeHTML(p.Category,p.ProductID,p.ProductName,p.UnitPrice,url);
+            }
         }
         //get array of product use index
         Object.entries(t.val())[0][1];
 
-
+        $('.set-bg').each(function () {
+            var bg = $(this).data('setbg');
+            $(this).css('background-image', 'url(' + bg + ')');
+        });
         //get imgs
        // await firebase.storage().ref("/images/CountDown/Product/product1.png").getDownloadURL();
         // final StorageReference storageRef = storage.getReference();
@@ -40,18 +49,16 @@ var indexControl = (function () {
 
        // console.log(t.val());
         
-        for(var i =0; i<6; i++){
-
-        }
+     
 
          return;
     }
 
-    function makeHTML(label ,pid , name ,  price ){
+    function makeHTML(label ,pid , name ,  price ,url){
         $("#setPopular").append(
         '<div class="col-lg-3 col-md-4 col-sm-6 mix '+label+'">' +
         '   <div class="featured__item">'+
-        '       <div class="featured__item__pic set-bg" data-setbg="firebase.storage().ref("/images/CountDown/Product/product1.png").getDownloadURL();">' +
+        '       <div class="featured__item__pic set-bg" data-setbg="'+url+'">' +
         '           <ul class="featured__item__pic__hover">' +
         '               <li><a ><i class="fa fa-heart"></i></a></li>' +
         '               <li><a ><i class="fa fa-retweet"></i></a></li>' +
@@ -59,8 +66,8 @@ var indexControl = (function () {
         '           </ul>' +
         '       </div>' +
         '       <div class="featured__item__text">' +
-        '           <h6><a >+name+</a></h6>' +
-        '           <h5>+price+</h5>' +
+        '           <h6><a >'+name+'</a></h6>' +
+        '           <h5>'+price+'</h5>' +
         '       </div>' +
         '   </div>' +
         '</div>' );
