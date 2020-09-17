3d4b62cb05dfcc5f8ba03e0ff160a97f05c895aa
Derek Zou <zoude603@student.otago.ac.nz>
2020-09-18 00:17:19 +1200

    debug

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -1,7 +1,7 @@
 var checkout = (function () {
 
     //global variables
-    var pub = {};
+    var pub = {},submitOrder;
     //function to validate user input data at the front end.
     /**
      * Check to email
@@ -21,65 +21,50 @@ var checkout = (function () {
 
     // function to submit an order details to the firebase.
     pub.submitOrder = function () {
-        var db = firebase.firestore();
-  
-  
-        var data = $('#checkout_form').serializeArray();
-  
-              //add to database
-              db.collection("orders").doc().set({
-                 first_name: data[0],
-                 last_name: data[1],
-                 email: data[2],
-                 address: data[3],
-                 uid:"guest",
-                 cart:"testing"
 
-              })
-                 .then(function (response) {
-                  //  alert("new data created!");
-                    console.log("guest order data created!");
-                    db.collection("order_user").doc("guest").collection("data").doc(response.id).set({
+        console.log("submited");
 
-                    });
-                    return true;
-                 })
-                 .catch(function (error) {
-                    alert("error messge: " + error.message);
-                    console.log("error messge: " + error.message);
-                 });
+        //var db = firebase.firestore();
   
   
-              console.log("order created for the guest user!");
-             // console.log("uidis:" + firebase.auth().currentUser.uid);
-              // return false;
-           
-           
-              
+        // var data = $('#checkout_form').serializeArray();
+        //       //add to database
+        //       db.collection("orders").doc({
+        //          first_name: data[0],
+        //          last_name: data[1],
+        //          email: data[2],
+        //          address: data[3],
+        //          uid:"guest",
+        //          cart:"testing"
 
+        //       })
+        //          .then(function () {
+        //           //  alert("new data created!");
+        //             console.log("guest order data created!"+response.id);
+        //             // db.collection("order_user").doc("guest").collection("data").doc(response.id).set({
+
+        //             // });
+        //             return false;
+        //          })
+        //          .catch(function (error) {
+        //             alert("error messge: " + error.message);
+        //             console.log("error messge: " + error.message);
+        //             return false;
+        //          });
   
-      
+  
+              //console.log("order created for the guest user!");
+    
         console.log(data);
-        return false;
-  
-  
-  
-     }
 
-    //public funtion
-    pub.functiona = function () {
-        firebase.auth().signOut()
-            .then(function () {
-                // Sign-out successful.
-            }).catch(function (error) {
-                // An error happened.
-            });
-    }
+        return false;
+       }
+
+
 
     //setup public
     pub.setup = function () {
-        //console.log("test");
-        //$('#login_form').submit(create);
+      console.log("checkout loaded");
 
     };
 
