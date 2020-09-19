3ef40e7428d1cf3f3e7f0b8c72591d609f819b3a
Vincent Toailoa <55169059+Raiiiden@users.noreply.github.com>
2020-09-19 12:24:13 +1200

    Leave Message Form
    Form is working and linking to database

--- public/js/contactForm.js
+++ public/js/contactForm.js
@@ -8,18 +8,21 @@ let messenger = document.querySelector("#msgSender");
 let msgEmail = document.querySelector("#msgEmail");
 let msgDetails = document.querySelector("#msgDetails");
 
-const db = firestore.collection("customerMessages");
+const db = firestore.collection("custMessages");
 
 submitBtn.addEventListener('click', function() {
+    console.log("It reaches here");
+
     let messengerInput = messenger.value;
     let msgEmailInput = msgEmail.value;
     let msgDetailsInput = msgDetails.value;
+    console.log(messenger.value);
 
     // Access the database collection
     db.doc().set({
-        email = msgEmailInput,
-        message = msgDetailsInput,
-        name = messengerInput
+        email : msgEmailInput,
+        message : msgDetailsInput,
+        name : messengerInput
     })
     .then(function(){
         console.log("Data Saved");
