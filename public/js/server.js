6428633302b5b5119d18ab10b3610dc3452372c6
Raiiiden <55169059+Raiiiden@users.noreply.github.com>
2020-10-01 17:31:29 +1300

    Stripe Payment Files
    Payment implementation in process. Added stripe files and currently finishing functionality

---
+++ public/js/server.js
@@ -0,0 +1,30 @@
+const stripe = require('stripe')('pk_test_51HXI5fJ7YxCSpAcZlx6bNpqzOP8vVbNmdpG5coZdR14Z9gq8pMsGQiDry3fX7wojLyfbISTrbog3yiId4tWhWDCJ00TtEEfE7u');
+const express = require('express');
+const app = express();
+app.use(express.static('.'));
+
+const YOUR_DOMAIN = 'http://localhost:5000';
+
+app.post('/create-session', async(req, res) => {
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: [{
+            price_data: {
+                currency: 'usd',
+                product_data: {
+                    name: 'Stubborn Attachments',
+                    images: ['https://i.imgur.com/EHyR2nP.png'],
+                },
+                unit_amount: 2000,
+            },
+            quantity: 1,
+        }, ],
+        mode: 'payment',
+        success_url: `${YOUR_DOMAIN}/success.html`,
+        cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+    });
+    res.json({
+        id: session.id
+    });
+});
+app.listen(5000, () => console.log('Running on port 5000'));
