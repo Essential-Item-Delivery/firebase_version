31c644a8875339bf2036ca7de44e79a8bb27848d
Usman Shah <shaus019@student.otago.ac.nz>
2020-10-09 14:53:58 +1300

    Added comments and clean the code at delivery.js file.

--- public/js/delievery.js
+++ public/js/delievery.js
@@ -1,78 +1,71 @@
-//const { findSourceMap } = require("module");
-
+/**
+ * Show the delivery and its status.
+ */
 var delievery = (function () {
 
     var pub = {};
-    var i=1;
-    var last=0;
-    function makeHTML4(one,two,three,four) {
+    var i = 1;
+    var last = 0;
+    function makeHTML4(one, two, three, four) {
 
         return ' <tr>' +
-            '       <th scope="row">'+one+'</th>' +
-            '       <td>'+two+'</td>' +
-            '      <td>'+three+'</td>' +
-            '     <td>'+four+'</td>' +
+            '       <th scope="row">' + one + '</th>' +
+            '       <td>' + two + '</td>' +
+            '      <td>' + three + '</td>' +
+            '     <td>' + four + '</td>' +
             '</tr>';
     }
-    function makeHTML5(one,two,three,four,five) {
+    function makeHTML5(one, two, three, four, five) {
 
         return ' <tr>' +
-            '       <th scope="row">'+one+'</th>' +
-            '       <td>'+two+'</td>' +
-            '      <td>'+three+'</td>' +
-            '     <td>'+four+'</td>' +
-            '     <td>'+five+'</td>' +
+            '       <th scope="row">' + one + '</th>' +
+            '       <td>' + two + '</td>' +
+            '      <td>' + three + '</td>' +
+            '     <td>' + four + '</td>' +
+            '     <td>' + five + '</td>' +
             '</tr>';
     }
 
-
-   
-  
     pub.showDelievery = async function (i) {
-      
+
         $("#dtable").find("tbody").html("");
         var orders = await datacontrol.getALLorders();
-        last=orders .length;
-        var shop ="Commerce Building, Dunedin, Otago 9016, New Zealand";
+        last = orders.length;
+        var shop = "Commerce Building, Dunedin, Otago 9016, New Zealand";
         $("#mapbox-directions-origin-input").children().children("input").val(shop);
 
-       
-       
-        // for(var i= 1; i<orders.length;i++){
-            //var i=1;
-            var cart =orders[i-1].cart;
-           var carthtml="<ul>" ;
-           for(var j= 0; j<cart.length;j++){
-            carthtml+="<li>ID:"+orders[i-1].cart[j].pid +" \t name:"+orders[i-1].cart[j].name+" x "+ orders[i-1].cart[j].unit+"</li>"
+        var cart = orders[i - 1].cart;
+        var carthtml = "<ul>";
+        for (var j = 0; j < cart.length; j++) {
+            carthtml += "<li>ID:" + orders[i - 1].cart[j].pid + " \t name:" + orders[i - 1].cart[j].name + " x " + orders[i - 1].cart[j].unit + "</li>"
 
-           }
+        }
 
-            var name =orders[i-1].first_name.value+" "+orders[i-1].last_name.value;
-            var address =orders[i-1].address.value;
-            var opeations =" <button class='complete'>Complete</button> <button>report</button>";
-            $("#dtable").find("tbody").append(makeHTML4(name,address , carthtml,opeations));
-        // }
+        var name = orders[i - 1].first_name.value + " " + orders[i - 1].last_name.value;
+        var address = orders[i - 1].address.value;
+        var opeations = " <button class='complete'>Complete</button> <button>report</button>";
+        $("#dtable").find("tbody").append(makeHTML4(name, address, carthtml, opeations));
         $("#mapbox-directions-destination-input").children().children("input").val(address);
-        
+
     }
 
-    pub.next = async function(){
-        if(i<last){
+    pub.next = async function () {
+        if (i < last) {
             alert("next order");
             i++;
             await pub.showDelievery(i);
             $(".complete").click(pub.next);
-        }else{
+        } else {
             alert("last order reached");
         }
-      
+
     }
 
     pub.setup = async function () {
-      
-       await pub.showDelievery(i);
+
+        await pub.showDelievery(i);
         $(".complete").click(pub.next);
-       
+
     }
 
     return pub;
