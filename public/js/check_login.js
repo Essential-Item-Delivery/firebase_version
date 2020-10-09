adacb813a3b67390b0decac02dcc0641f8efe889
Raiiiden <55169059+Raiiiden@users.noreply.github.com>
2020-10-09 15:10:05 +1300

    Check_login.js
    Check login styles fixed. No issues.

--- public/js/check_login.js
+++ public/js/check_login.js
@@ -1,32 +1,30 @@
 /**
  * Check if the user is logged in and displays user name and a button to logout, if the user is loggged in.
  */
-firebase.auth().onAuthStateChanged(function (user) {
+firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
 
         logcheck.displayUser();
 
-    } else {
-    }
+    } else {}
 });
-var logcheck = (function () {
+var logcheck = (function() {
 
     var pub = {};
 
-    pub.displayUser = async function () {
+    pub.displayUser = async function() {
         $("#loggerRemove").remove();
         $("#createrRemove").remove();
 
         var uid = firebase.auth().currentUser.uid;
 
-        $("#logger").html('<i class=\"fa fa-user\">Welcome ' + await datacontrol.getUserName(uid) + '</i> <button onclick="logcheck.logout()">Log Out</button>');
+        $("#logger").html('<i class style="font-family:Helvetica Neue, Helvetica, Arial, sans-serif;font-size:14px;font-style:normal;"\"fa fa-user\"> Hi ' + await datacontrol.getUserName(uid) + '</i> <button class="btn btn-outline-btn btn-outline-secondary btm-sm btn-sm" onclick="logcheck.logout()">Log Out</button>');
     }
-    pub.logout = function () {
+    pub.logout = function() {
         firebase.auth().signOut()
-            .then(function () {
+            .then(function() {
                 location.href = 'index.html';
-            }).catch(function (error) {
-            });
+            }).catch(function(error) {});
     }
     return pub;
 
