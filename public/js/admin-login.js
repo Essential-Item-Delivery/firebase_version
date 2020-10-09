8b6657bbc29e92d3262e1ec63b772af488dfc85b
Usman Shah <shaus019@student.otago.ac.nz>
2020-10-09 13:40:47 +1300

    Commented about us and admin js files..

--- public/js/admin-login.js
+++ public/js/admin-login.js
@@ -1,59 +1,20 @@
-// Start grabbing our DOM Objects
-
-// initialize firebase auth
+/**
+ * Initialize firebase authentication.
+ * and give access to only authorized staff.
+ * */
 var auth = firebase.auth();
-
-// var email = document.getElementById('email');
-// var password = document.getElementById('password');
 var btnLogin = document.getElementById('btnLogin');
 var btnLogout = document.getElementById('btnLogout');
 
-// action response to click
-// btnLogin.addEventListener('click', funcLogin);
-// btnLogout.addEventListener('click', funcLogout);
 
 function funcLogin() {
     var email = document.getElementById('email');
     var password = document.getElementById('password');
-    
-    if(email.value==='admin'){
+
+    if (email.value === 'admin') {
         window.location.replace("admin-panel.html");
-    }else{
+    } else {
         alert("access denied");
     }
-    // firebase.auth().signInWithEmailAndPassword(email.value, password.value)
-    //     .then(function(data) {
-    //         console.log(email.value);
-    //         console.log("Login Success");
-    //         console.log(data);
-    //         location.href = 'admin-panel.html';
-    //         //location.reload();
-    //     })
-    //     .catch(function(error) {
-    //         alert(error);
-    //     })
+
 }
-
-// firebase.auth().onAuthStateChanged(function(user) {
-//     if (user) {
-//         // User is signed in.
-//         btnLogout.classList.remove('hidden');
-
-
-//     } else {
-//         // User is signed out.
-//         btnLogout.classList.add('hidden');
-//         btnLogin.classList.remove('hidden');
-//     }
-// })
-
-// // function logout
-// function funcLogout() {
-//     console.log("Button Clicked");
-//     auth.signOut().then(function() {
-//         console.log("You are logged out");
-//         location.href = 'admin-login.html';
-//     }).catch(function(err) {
-//         alert(err)
-//     })
-// }
